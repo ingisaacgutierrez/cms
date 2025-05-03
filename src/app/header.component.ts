ea75da53d16d14e523af36a743931ed67cf49fc9
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2025-05-02 21:36:00 -0500

    add nav bar and firsts tep to add components

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'cms-header',
   standalone: false,
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
 
