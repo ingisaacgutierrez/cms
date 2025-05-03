0bfe6601ce292ef0f3cce0052062fc244aadc51a
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2025-05-02 22:57:25 -0500

    component list and component details

--- src/app/contacts/contact-detail/contact-detail.component.ts
+++ src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
   standalone: false,
   templateUrl: './contact-detail.component.html',
-  styleUrl: './contact-detail.component.css'
 })
 export class ContactDetailComponent {
-
+  contact: Contact = new Contact('', '', '', '', '', null);
 }
