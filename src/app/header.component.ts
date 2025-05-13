d01aff271fc2ba63dcb76d758142c536490b2bd5
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2025-05-13 00:43:29 -0500

    header component changes

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
   standalone: false,
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  @Output() selectedFeatureEvent = new EventEmitter<string>();
+
+  onSelected(selectedEvent: string) {
+    this.selectedFeatureEvent.emit(selectedEvent);
+  }
 }
