320a61c92f3dadb3ab0a73efd00eb3938d6283f5
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2025-05-21 23:08:55 -0500

    Injecting and Using the Contact Service

--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,37 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
-
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-list',
   standalone: false,
   templateUrl: './contact-list.component.html',
 })
-
-export class ContactListComponent {
-  contacts: Contact[] = [
-    new Contact(
-      '1',
-      'R. Kent Jackson',
-      'jacksonk@byui.edu',
-      '208-496-3771',
-      '../../assets/images/jacksonk.jpg',
-      null
-    ),
-    new Contact(
-      '2',
-      'Rex Barzee',
-      'barzeer@byui.edu',
-      '208-496-3768',
-      '../../assets/images/barzeer.jpg',
-      null
-    )
-  ];
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
 
   @Output() selectedContactEvent = new EventEmitter<Contact>();
 
-    onSelected(contact: Contact) {
-        this.selectedContactEvent.emit(contact);
-    }
+  constructor(private contactService: ContactService) {}
 
+  ngOnInit(): void {
+    this.contacts = this.contactService.getContacts();
+  }
+
+  onSelected(contact: Contact) {
+    this.selectedContactEvent.emit(contact);
+  }
 }
