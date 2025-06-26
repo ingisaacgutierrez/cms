4e77d44d24bb169029761170548f8f74a296bb51
ingisaacgutierrez <igutierrezsaltos@gmail.com>
2025-06-26 01:39:32 -0500

    update with node js/express project

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const index = require('./server/routes/app');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(logger('dev'));
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  next();
+});
+
+
+app.use(express.static(path.join(__dirname, 'dist/cms/browser')));
+
+
+app.use('/', index);
+
+
+app.get('/*splat', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/cms/browser/index.html'));
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, () => {
+  console.log(`API running on localhost:${port}`);
+});
+
+
